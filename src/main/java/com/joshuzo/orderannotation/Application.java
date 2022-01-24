ab5fce3d64e7b84738ee5d1dddf510aa1ddd33c3
Josh <josh@joshuzo.com>
2022-01-25 00:01:38 +1100

    Order annotation demo

--- src/main/java/com/joshuzo/orderannotation/Application.java
+++ src/main/java/com/joshuzo/orderannotation/Application.java
@@ -12,16 +12,6 @@ interface FileFormatHandler {
 }
 
 @Service
-@Order(1)
-class BinaryFileService implements FileFormatHandler {
-
-  @Override
-  public void execute() {
-    System.out.println("Handled Binary File");
-  }
-}
-
-@Service
 @Order(10)
 class ASCIIFileService implements FileFormatHandler {
 
@@ -32,24 +22,43 @@ class ASCIIFileService implements FileFormatHandler {
 }
 
 @Service
+@Order(1)
+class BinaryFileService implements FileFormatHandler {
+
+  @Override
+  public void execute() {
+    System.out.println("Handled Binary File");
+  }
+}
+
+@Service
 @RequiredArgsConstructor
 class FileManager {
 
   private final List<FileFormatHandler> handlers;
 
   void handle() {
-    handlers.forEach(handler -> System.out.println(handler.getClass()));
+    handlers.forEach(
+        handler -> {
+          System.out.println(handler.getClass());
+          handler.execute();
+        });
   }
 }
 
 public class Application {
 
+  public static final String BASE_PACKAGE = "com.joshuzo.orderannotation";
+
   public static void main(String[] args) {
-    ApplicationContext context =
-        new AnnotationConfigApplicationContext("com.joshuzo.orderannotation");
-    ASCIIFileService asciiFileService = context.getBean(ASCIIFileService.class);
+    ApplicationContext context = new AnnotationConfigApplicationContext(BASE_PACKAGE);
     FileManager fileManager = context.getBean(FileManager.class);
-    asciiFileService.execute();
     fileManager.handle();
   }
 }
+
+// OUTPUT
+// class com.joshuzo.orderannotation.BinaryFileService
+// Handled Binary File
+// class com.joshuzo.orderannotation.ASCIIFileService
+// Handled ASCII File
